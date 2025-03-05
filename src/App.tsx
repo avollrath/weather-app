a5240f2d7aa84061caf36346434bfd7dcbc78920
Andre Vollrath <andre.vollrath@talentadore.com>
2025-03-05 23:02:32 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { BrowserRouter } from "react-router-dom"
+import Layout from './components/layout'
+
+function App() {
+
+  return (
+      <BrowserRouter>
+        <Layout>
+           Hello
+        </Layout>
+      </BrowserRouter>
+    
+  )
+}
+
+export default App
